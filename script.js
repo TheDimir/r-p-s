c06b9d39ccea4948271ad5aa8b6425da88efa088
TheDimir <d.r.lazav@gmail.com>
2022-12-31 11:46:37 +0200

    Add player choice

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 //get computer choice at random
-let computerSelection = 0;
-function getCompChoice() {
-    let compValues = ["rock","paper","scissors"],
-        valueToUse = compValues[Math.floor(Math.random() * compValues.length)];
-    return valueToUse;
-}
+
+//let computerSelection = 0;
+//function getCompChoice() {
+    //let compValues = ["rock","paper","scissors"],
+        //valueToUse = compValues[Math.floor(Math.random() * compValues.length)];
+    //return valueToUse;
+//}
 //assign it to variable
-computerSelection = getCompChoice();
+
+//computerSelection = getCompChoice();
+
 //ask for player choice
+let playerSelection = 0;
+function getPlayerChoice() {
+    let playerChoice = prompt('Make your choice.', '');
+    if (playerChoice == 'Rock') {
+        playerChoice = 'Rock';
+    } else if (playerChoice == 'Paper') {
+        playerChoice = 'Paper';
+    } else if (playerChoice == 'Scissors') {
+        playerChoice = 'Scissors';  
+    }  else {
+        playerChoice = 'Not a valid choice';
+    }
+    return playerChoice
+}
+
+playerSelection = getPlayerChoice();
 //compare choices from player and comp
 //alert winner of the round
 //count results of each round
