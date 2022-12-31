64e9552820299dcb717dc31d787b7a660cd92c2d
TheDimir <d.r.lazav@gmail.com>
2022-12-31 12:22:32 +0200

    Make better player choice selection

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
 //get computer choice at random
 
-//let computerSelection = 0;
-//function getCompChoice() {
-    //let compValues = ["rock","paper","scissors"],
-        //valueToUse = compValues[Math.floor(Math.random() * compValues.length)];
-    //return valueToUse;
-//}
+let computerSelection = 0;
+function getCompChoice() {
+    let compValues = ["rock","paper","scissors"]
+    let valueToUse = compValues[Math.floor(Math.random() * compValues.length)];
+    return valueToUse;
+}
 //assign it to variable
 
-//computerSelection = getCompChoice();
+computerSelection = getCompChoice();
 
 //ask for player choice
 let playerSelection = 0;
 function getPlayerChoice() {
-    let playerChoice = prompt('Make your choice.', '');
-    if (playerChoice == 'Rock') {
-        playerChoice = 'Rock';
-    } else if (playerChoice == 'Paper') {
-        playerChoice = 'Paper';
-    } else if (playerChoice == 'Scissors') {
-        playerChoice = 'Scissors';  
-    }  else {
-        playerChoice = 'Not a valid choice';
+    const playerChoice = prompt('Make your choice.', '').toLowerCase();
+    const possibleChoices = ['rock', 'paper', 'scissors']
+    if (possibleChoices.includes(playerChoice)) {
+        return playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1)
+    } else {
+        return 'Not a valid choice';
     }
-    return playerChoice
+}
+//assign it to variable
+playerSelection = getPlayerChoice();
+
+//compare choices from player and comp
+function game() {
+    
 }
 
-playerSelection = getPlayerChoice();
-//compare choices from player and comp
 //alert winner of the round
 //count results of each round
 //reapeat for 5 rounds
