63b51e1df1f29e008cc806c70d9f81fe31e7d814
TheDimir <d.r.lazav@gmail.com>
2023-01-03 12:20:15 +0200

    Add game process

--- script.js
+++ script.js
@@ -16,20 +16,63 @@ function getPlayerChoice() {
     const playerChoice = prompt('Make your choice.', '').toLowerCase();
     const possibleChoices = ['rock', 'paper', 'scissors']
     if (possibleChoices.includes(playerChoice)) {
-        return playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1)
+        return playerChoice;
     } else {
         return 'Not a valid choice';
     }
 }
 //assign it to variable
 playerSelection = getPlayerChoice();
+//compare choices and declare winner
+let winner;
 
-//compare choices from player and comp
-function game() {
-    
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return 'Tie';
+    }
+    if (playerSelection === 'rock') {
+        if (computerSelection === 'paper') {
+            return 'W for comp';
+        } else {
+            return 'W for ya';
+        }
+    }
+    if (playerSelection === 'paper') {
+        if (computerSelection === 'scissors') {
+            return 'W for comp';
+        } else {
+            return 'W for ya';
+        }
+    }
+    if (playerSelection === 'scissors') {
+        if (computerSelection === 'rock') {
+            return 'W for comp';
+        } else {
+            return 'W for ya';
+        }
+    }
 }
 
-//alert winner of the round
-//count results of each round
-//reapeat for 5 rounds
-//alert winner
+winner = playRound(playerSelection, computerSelection);
+//reapeat for 5 rounds and update scores
+let playerScore = 0;
+let computerScore = 0;
+let round = 1;
+
+function game() {
+    for (i = 1; i <= 5; i++) {
+        playRound(playerSelection, computerSelection);
+
+        if (winner === 'Tie') {
+            console.log(winner, round++);
+        }
+        if(winner === 'W for ya') {
+            console.log(winner, playerScore++, round++);
+        } 
+        if (winner === 'W for comp') {
+            console.log(winner, computerScore++, round++);
+        } 
+    }
+}
+
+console.log(game());
